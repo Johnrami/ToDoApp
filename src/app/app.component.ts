0c26b4464d7a2f7b164dc6cd518243bea1f6d3ed
Unknown <johnram97@live.com>
2018-07-07 23:21:56 -0500

    Just made my ToDoApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  todoInput = '';
+  todos = [];
+
+  createTodo() {
+    this.todos.push(this.todoInput);
+    this.todoInput = "";
+  }
+
+  editTodo(chore) {
+    console.log(chore);
+    let index = this.todos.indexOf(chore);
+    console.log(index);
+
+    this.todos[index] = prompt('please write new todo');
+  }
+
+  deleteTodo(chore) {
+    let index = this.todos.indexOf(chore);
+    this.todos.splice(index, 1);
+  }
+  
+
 }
+
+
